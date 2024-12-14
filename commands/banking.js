d345f81593d5c0bec9fb8dfbc245a034208767d7
Nikhil Das <nknikhilporuppath@gmail.com>
2024-12-14 19:30:36 +0530

    Updated banking with new updates

--- commands/banking.js
+++ commands/banking.js
@@ -9,42 +9,76 @@ const mongo_client = new MongoClient(process.env.MONGO_CON_URL, {
 	},
 });
 
+function parseNumtoComma(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('withdraw')
 		.setDescription('Send request to withdraw Money from faction Bank')
-		.addIntegerOption(option =>
+		.addStringOption(option =>
 			option.setName('amount')
 				.setDescription('Amount to Withdraw')
 				.setRequired(true)),
 	async execute(interaction) {
-		const withdrawAmount = interaction.options.getInteger('amount');
-		const sendMoney = new ButtonBuilder()
-			.setURL('https://www.torn.com/factions.php?step=your&type=1#/tab=controls&option=give-to-user')
-			.setLabel('Send')
-			.setStyle(ButtonStyle.Link);
-		const sendMoneyRow = new ActionRowBuilder()
-			.addComponents(sendMoney);
-		await mongo_client.connect();
-		const usersCol = mongo_client.db('fas-bot').collection('users');
-		const userData = await usersCol.findOne({ discordUserID: interaction.user.id });
-		const userBalance = await checkUserBalance(userData.tornUserId);
+		const withdrawAmountStr = interaction.options.getString('amount');
+		const amountPower = withdrawAmountStr.slice(-1);
+		let withdrawAmount = 0;
+		if (amountPower === 'K' || amountPower === 'k') {
+			withdrawAmount = parseFloat(withdrawAmountStr.slice(0, -1)) * 1000;
+		}
+		else if (amountPower === 'M' || amountPower === 'm') {
+			withdrawAmount = parseFloat(withdrawAmountStr.slice(0, -1)) * 1000000;
+		}
+		else if (amountPower === 'B' || amountPower === 'b') {
+			withdrawAmount = parseFloat(withdrawAmountStr.slice(0, -1)) * 1000000000;
+		}
+		else if (!isNaN(withdrawAmountStr)) {
+			withdrawAmount = parseFloat(withdrawAmountStr);
+		}
+		else if (withdrawAmountStr == 'all') {
+			withdrawAmount = 'all';
+		}
+		if (withdrawAmount === 0) {
+			await interaction.reply('Invalid request!');
+		}
+		else {
+			await mongo_client.connect();
+			const usersCol = mongo_client.db('fas-bot').collection('users');
+			const userData = await usersCol.findOne({ discordUserID: interaction.user.id });
+			const userBalance = await checkUserBalance(userData.tornUserId);
+			if (withdrawAmount === 'all') {
+				withdrawAmount = userBalance;
+			}
+			if (userBalance < withdrawAmount) {
+				await interaction.reply('You have don\'t have enough money to withdraw!!!');
+			}
+			else {
+				const sendMoney = new ButtonBuilder()
+					.setURL('https://www.torn.com/factions.php?step=your&type=1#/tab=controls&option=give-to-user')
+					.setLabel('Send')
+					.setStyle(ButtonStyle.Link);
+				const sendMoneyRow = new ActionRowBuilder()
+					.addComponents(sendMoney);
+				const channel = await interaction.member.guild.channels.cache.get('1315276631555833886');
+				const alertMsg = await channel.send({
+					content: `${userData.tornUserName} has requested ${parseNumtoComma(withdrawAmount)} from Faction Bank. They have a balance of ${parseNumtoComma(userBalance)}`,
+					components: [sendMoneyRow],
+				});
+				const trxInfo = {
+					messageId: alertMsg.id,
+					userName: userData.tornUserName,
+					amount: withdrawAmount,
+					status: 0,
 
-		const channel = await interaction.member.guild.channels.cache.get('1315276631555833886');
-		const alertMsg = await channel.send({
-			content: `${userData.tornUserName} has requested ${withdrawAmount} from Faction Bank. They have a balance of ${userBalance}`,
-			components: [sendMoneyRow],
-		});
-		const trxInfo = {
-			messageId: alertMsg.id,
-			userName: userData.tornUserName,
-			amount: withdrawAmount,
-			status: 0,
-
-		};
-		const bankTransactionCol = mongo_client.db('fas-bot').collection('bankTransactions');
-		await bankTransactionCol.insertOne(trxInfo);
-		await mongo_client.close();
-		await interaction.reply('Your request has been placed, Please wait for the Bankers to approve!');
+				};
+				const bankTransactionCol = mongo_client.db('fas-bot').collection('bankTransactions');
+				await bankTransactionCol.insertOne(trxInfo);
+				await mongo_client.close();
+				await interaction.reply('Your request has been placed, Please wait for the Bankers to approve!');
+			}
+		}
 	},
 };
